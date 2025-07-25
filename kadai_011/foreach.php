1de19df8a5e561aa0e6ddd3d5c95a7532bc504bb
minami0018 <minami0018@icloud.com>
2025-07-25 18:29:04 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<head>
+<meta charset="UTF-8">
+  <title>PHP基礎編</title>
+  </head>
+
+<body>
+  <p>
+    <?php
+    $item = ['name'=> 'onion', 'price'=> 200,'origin'=> '北海道'];
+    // 連想配列の値を出力する
+    echo "$item.<br>";
+    ?>
+  </p>
+  </body>
+</html>
