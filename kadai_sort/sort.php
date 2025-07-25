719bf121956a69c47f2145161fbe174dbbee0432
minami0018 <minami0018@icloud.com>
2025-07-25 22:49:29 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -10,7 +10,8 @@
     <p>
         <?php
        function sort_2way($array,$order){
-        if ($order === 'asc') {
+        if ($order === 'true' ){
+
             sort($array);
             echo "昇順にソートします。<br> ";
         } else {
@@ -21,13 +22,13 @@
         foreach ($array as $num) {
             echo "{$num} <br>";
         }
-      }
+       }
 
         // 配列を用意する
         $nums = [15,4,18,23,10];
-        sort_2way($nums,'asc');
+        sort_2way($nums,'true');
         echo '<br>';
-        sort_2way($nums,'desc');
+        sort_2way($nums,'false');
        
         ?>
     </p>
